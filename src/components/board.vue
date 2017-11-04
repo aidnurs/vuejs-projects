174a489ea4c924614e4cd9d46a9c6a567c946c36
aidnurs <nursultan_imanov@mail.ru>
2017-11-04 23:04:00 +0100

    finished game algorithm

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,13 +1,16 @@
 <template lang="html">
   <div class="" >
-    <button
-    @click="(trueStory)||(arr.length===1)?addNumber(but):null"
-    v-for="(but,index) in buttons"
-    ref="refs"
-    v-bind:id="'color'+(++index)">{{but}}</button>
+    <button @click="addNumber(1)" v-bind:class="{ active: isBoxOneActive}">1</button>
+    <button @click="addNumber(2)" v-bind:class="{ active: isBoxTwoActive}">2</button>
+    <button @click="addNumber(3)" v-bind:class="{ active: isBoxThreeActive}">3</button>
+    <button @click="addNumber(4)" v-bind:class="{ active: isBoxFourActive}">4</button>
     {{arr}}
     {{input}}
     {{trueStory}}
+    <button @click="start">start</button>
+    <!--<button @click="restart">restart</button>-->
+    <button @click="stop">stop</button>
+
   </div>
 </template>
 
@@ -15,22 +18,42 @@
 export default {
   data() {
     return {
-      buttons: [1, 2, 3, 4],
       input: [],
-      number: 0,
       arr: [],
-      trueStory: false
+      number: 0,
+      trueStory: true,
+      userTurn: false,
+      isBoxOneActive: false,
+      isBoxTwoActive: false,
+      isBoxThreeActive: false,
+      isBoxFourActive: false,
+      timer:0,
     };
   },
   mounted() {
-    this.generate();
-    this.showNumbers();
   },
   methods: {
-    addNumber(a) {
-      this.input.push(a);
-      this.check();
+    start() {
+      this.generate();
+      this.userTurn = true;
       this.showNumbers();
+    },
+    restart() {
+      this.arr = [];
+      this.trueStory = true;
+      this.start();
+    },
+    stop(){
+      this.arr = [];
+      this.input=[];
+      this.trueStory = true;
+    },
+    addNumber(a) {
+      if (this.trueStory&&this.userTurn) {
+        this.input.push(a);
+        this.check();
+
+      }
     },
     check() {
       for (var i = 0; i < this.input.length; i++) {
@@ -38,40 +61,71 @@ export default {
           this.trueStory = true;
         } else {
           this.trueStory = false;
+          this.input = [];
           break;
         }
       }
       if (this.arr.length === this.input.length) {
         this.input = [];
         this.generate();
+        this.showNumbers();
       }
     },
     generate() {
-      this.number = Math.floor(Math.random() * 4) + 1;
-      this.arr.push(this.number);
-      this.number = 0;
+      if (this.trueStory) {
+        this.number = Math.floor(Math.random() * 4) + 1;
+        this.arr.push(this.number);
+        this.number = 0;
+      }
     },
     showNumbers() {
-      /*function doSetTimeout(i) {
-        setTimeout(function() {
-          elem[[i]].classList.remove('big');
-          //console.log(elem[i]);
-        }, 1000);
+      this.userTurn=false;
+      var self = this;
+      var i = 0;
+      self.timer = setInterval(function() {
+        if (i >= self.arr.length) {
+          self.stopInterval();
+        }
+        self.clickEffect(self.arr[i]);
+        i++;
+      }, 700);
+    },
+    stopInterval: function() {
+      clearInterval(this.timer);
+      this.userTurn=true;
+    },
+    clickEffect: function(boxNum) {
+      var self = this;
+      switch (boxNum) {
+        case 1:
+          this.isBoxOneActive = true;
+          setTimeout(function() {
+            self.isBoxOneActive = false;
+          }, 300);
+          break;
+        case 2:
+          this.isBoxTwoActive = true;
+          //audio2.play();
+          setTimeout(function() {
+            self.isBoxTwoActive = false;
+          }, 300);
+          break;
+        case 3:
+          this.isBoxThreeActive = true;
+          //audio3.play();
+          setTimeout(function() {
+            self.isBoxThreeActive = false;
+          }, 300);
+          break;
+        case 4:
+          this.isBoxFourActive = true;
+          //audio4.play();
+          setTimeout(function() {
+            self.isBoxFourActive = false;
+          }, 300);
+          break;
       }
-      var elem = this.$refs.refs;
-      for (var i = 0; i < this.arr.length; ++i) {
-        //console.log(elem[this.arr[i]-1]);
-        //console.log(this.arr[i]);
-        elem[this.arr[i] - 1].classList.add('big');
-        doSetTimeout(this.arr[i] - 1);
-      }*/
-      for (var i = 0; i < this.arr.length; i++) {
-        var elem = this.$refs.refs[this.arr[i] - 1];
-        elem.classList.add('big');
-        setTimeout(function() {
-          elem.classList.remove('big');
-        }, (  i+1)*1000);
-      }
+      return;
     }
   }
 };
@@ -102,5 +156,8 @@ button:active{
 .big{
   width: 100px;
   height: 100px;
+}
+.active {
+  background-color: hsl(50, 50%, 10%);
 }
 </style>
